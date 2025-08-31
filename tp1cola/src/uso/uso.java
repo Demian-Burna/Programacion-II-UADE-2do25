b7296f82f823393dc03382b9f4305ddae7e8162f
Burna <XDXB23@domainba.com>
2025-08-31 13:49:31 -0300

    Avance en TP 1 Cola

--- tp1cola/src/uso/uso.java
+++ tp1cola/src/uso/uso.java
@@ -1,12 +1,13 @@
 package uso;
 
+import imple.Pila;
 import implementacion.Cola;
 
 public class uso{
 	
-public static void llenarCola(Cola cola) {
+	public static void llenarCola(Cola cola) {
 		
-		for(int i = 0; i <= 5; i++) {
+		for(int i = 0; i < 5; i++) {
 			cola.acolar(i);	
 		}
 	
@@ -15,20 +16,51 @@ public static void llenarCola(Cola cola) {
 	public static void mostrarCola(Cola cola) {
 		
 		Cola Colaaux = new Cola();
+		
 		Colaaux.inicializarCola();
-		do {
+		
+		while(!cola.colaVacia()) {
 			int elemento = cola.primero();
 			System.out.print("Elemento: " + elemento + "\n");
 			Colaaux.acolar(elemento);
 			cola.desacolar();
-		}
-		while(!cola.colaVacia());
+		};
 		
-		do {
+		while(!Colaaux.colaVacia()) {
 			cola.acolar(Colaaux.primero());
 			Colaaux.desacolar();
+		};
+		
+	}
+	
+	public static Cola pasarCola(Cola cola) {
+		
+		Cola colanueva = new Cola();
+		colanueva.inicializarCola();
+		
+		while(!cola.colaVacia()) {
+			int elemento = cola.primero();
+			colanueva.acolar(elemento);
+			cola.desacolar();
+		};
+		
+		return colanueva;
+	}
+	
+	public static void invertirCola1 (Cola cola) {		
+		
+		Pila pilaux = new Pila();
+		pilaux.inicializarPila();
+		
+		while(!cola.colaVacia()) {
+			pilaux.apilar(cola.primero());
+			cola.desacolar();
 		}
-		while(!Colaaux.colaVacia());
+		
+		while(!pilaux.pilaVacia()) {
+			cola.acolar(pilaux.tope());
+			pilaux.desapilar();
+		}
 		
 	}
 	
@@ -36,6 +68,22 @@ public static void llenarCola(Cola cola) {
 		
 		Cola cola1 = new Cola();
 		cola1.inicializarCola();
+		llenarCola(cola1);
+		
+		System.out.println("\nMostrando cola original\n");
+		mostrarCola(cola1);
+		
+		Cola cola2 = pasarCola(cola1);
+		System.out.println("\nMostrando cola 2\n");
+		mostrarCola(cola2);
+		
+		cola1 = pasarCola(cola2);
+		System.out.println("\nMostrando cola original\n");
+		mostrarCola(cola1);
+		
+		invertirCola1(cola1);
+		System.out.println("\nMostrando cola invertida\n");
+		mostrarCola(cola1);
 		
 		
 	}
