963fed637a9d8718ba4a2a80c5c8a65ddabd86bf
XDXB23 <XDXB23@Equipo035736.DOMAINBA.COM>
2025-09-01 14:38:06 -0300

    Actualización de TPS

--- tp1cola/src/uso/uso.java
+++ tp1cola/src/uso/uso.java
@@ -8,22 +8,28 @@ public class uso{
 	public static void llenarCola(Cola cola) {
 		
 		for(int i = 0; i < 5; i++) {
-			cola.acolar(i);	
+			cola.acolar((int)(Math.random() * 101));	
 		}
 	
 	}
 
+	public static void vaciarCola(Cola cola) {	
+		while(!cola.colaVacia()) {cola.desacolar();}	
+	}
+	
 	public static void mostrarCola(Cola cola) {
 		
 		Cola Colaaux = new Cola();
 		
 		Colaaux.inicializarCola();
-		
+		int indice = 1;
 		while(!cola.colaVacia()) {
 			int elemento = cola.primero();
-			System.out.print("Elemento: " + elemento + "\n");
+			
+			System.out.print("Elemento N°" + indice + ": " + elemento + "\n");
 			Colaaux.acolar(elemento);
 			cola.desacolar();
+			indice++;
 		};
 		
 		while(!Colaaux.colaVacia()) {
@@ -47,6 +53,31 @@ public class uso{
 		return colanueva;
 	}
 	
+	public static Cola copiarCola(Cola cola) {
+		
+		Cola Colaaux = new Cola();
+		Cola Colacopia = new Cola();
+		Colaaux.inicializarCola();
+		Colacopia.inicializarCola();
+		int elemento = 0;
+
+		while(!cola.colaVacia()) {
+			elemento = cola.primero();
+			Colaaux.acolar(elemento);
+			Colacopia.acolar(elemento);
+			cola.desacolar();
+
+		};
+		
+		while(!Colaaux.colaVacia()) {
+			cola.acolar(Colaaux.primero());
+			Colaaux.desacolar();
+		};
+		
+		return Colacopia;
+		
+	}
+	
 	public static void invertirCola1 (Cola cola) {		
 		
 		Pila pilaux = new Pila();
@@ -64,6 +95,105 @@ public class uso{
 		
 	}
 	
+	public static void invertirCola2 (Cola cola) {		
+		
+		int elementos = 0 ;
+		Cola Colaaux = new Cola();
+		Colaaux.inicializarCola();
+		
+		while(!cola.colaVacia()) {
+			Colaaux.acolar(cola.primero());
+			elementos++;
+			cola.desacolar();
+		}
+		
+		int[] arregloaux = new int[elementos];
+		
+		for(int i = 0; i < elementos; i++ ) {
+			arregloaux[i] = Colaaux.primero();
+			Colaaux.desacolar();
+		}
+		
+		for(int i = elementos - 1; i >= 0 ; i--) {
+			cola.acolar(arregloaux[i]);
+		}
+		
+	}
+	
+	public static void igualFinal(Cola cola1 , Cola cola2){
+		
+		Cola colaaux = new Cola();
+		colaaux.inicializarCola();
+		int ultimoelemento1 = 0;
+		int ultimoelemento2 = 0;
+		
+		while(!cola1.colaVacia()) {
+			ultimoelemento1 = cola1.primero();
+			colaaux.acolar(ultimoelemento1);
+			cola1.desacolar();
+		}
+		
+		while(!colaaux.colaVacia()) {
+			cola1.acolar(colaaux.primero());
+			colaaux.desacolar();
+		}
+		
+		while(!cola2.colaVacia()) {
+			ultimoelemento2 = cola2.primero();
+			colaaux.acolar(ultimoelemento2);
+			cola2.desacolar();
+		}
+			
+		while(!colaaux.colaVacia()) {
+			cola2.acolar(colaaux.primero());
+			colaaux.desacolar();
+		}
+		
+		if(ultimoelemento1 == ultimoelemento2) {
+			System.out.println("El elemento final de la cola1 (" + ultimoelemento1 + ") es igual al de la cola 2 (" + ultimoelemento2 + ")\n");
+		}
+		else {
+			System.out.println("\nEl elemento final de la cola1 (" + ultimoelemento1 + ") NO es igual al de la cola 2 (" + ultimoelemento2 + ")\n");
+		}
+		
+	}
+	
+	public static boolean capicua(Cola cola) {
+		
+		Cola colaaux1 = copiarCola(cola);
+		Cola colaaux2 = copiarCola(cola);
+		invertirCola1(colaaux1);
+		
+		while(!colaaux1.colaVacia()) {
+			if(!(colaaux2.primero() == colaaux1.primero())) {
+				return false;
+			}
+			colaaux1.desacolar();
+			colaaux2.desacolar();
+		}
+		
+		return true;
+		
+	}
+	
+	public static boolean inversa(Cola cola1, Cola cola2) {
+		
+		Cola colaaux1 = copiarCola(cola1);
+		Cola colaaux2 = copiarCola(cola2);
+		invertirCola1(colaaux2);
+		
+		while(!colaaux1.colaVacia()) {
+			if(!(colaaux1.primero() == colaaux2.primero())) {
+				return false;
+			}
+			colaaux1.desacolar();
+			colaaux2.desacolar();
+		}
+		
+		return true;
+		
+	}
+	
 	public static void main(String[] arg) {
 		
 		Cola cola1 = new Cola();
@@ -73,17 +203,99 @@ public class uso{
 		System.out.println("\nMostrando cola original\n");
 		mostrarCola(cola1);
 		
+		// Punto a
 		Cola cola2 = pasarCola(cola1);
-		System.out.println("\nMostrando cola 2\n");
+		System.out.println("\nMostrando cola 2 (Pasada)\n");
 		mostrarCola(cola2);
-		
 		cola1 = pasarCola(cola2);
 		System.out.println("\nMostrando cola original\n");
 		mostrarCola(cola1);
 		
+		// Punto b
 		invertirCola1(cola1);
-		System.out.println("\nMostrando cola invertida\n");
+		System.out.println("\nMostrando cola invertida1\n");
 		mostrarCola(cola1);
+		
+		// Punto c
+		invertirCola2(cola1);
+		System.out.println("\nMostrando cola invertida2\n");
+		mostrarCola(cola1);
+		
+		// Punto d
+		llenarCola(cola2);
+		System.out.println("\nMostrando cola1\n");
+		mostrarCola(cola1);	
+		System.out.println("\nMostrando cola2\n");
+		mostrarCola(cola2);
+		igualFinal(cola1,cola2);
+		
+		// punto e 
+		vaciarCola(cola1);
+		llenarCola(cola1);
+		
+		System.out.println("\nMostrando cola para capicua Negativo\n");
+		mostrarCola(cola1);
+		
+		if(capicua(cola1)) {
+			System.out.println("\nLa cola es capicua");
+		}
+		else {
+			System.out.println("\nLa cola NO es capicua");
+		}
+		
+		vaciarCola(cola1);
+		cola1.acolar(10);
+		cola1.acolar(20);
+		cola1.acolar(30);
+		cola1.acolar(30);
+		cola1.acolar(20);
+		cola1.acolar(10);
+		
+		System.out.println("\nMostrando cola para capicua Afirmativo\n");
+		mostrarCola(cola1);
+		
+		if(capicua(cola1)) {
+			System.out.println("\nLa cola es capicua");
+		}
+		else {
+			System.out.println("\nLa cola NO es capicua");
+		}
+		
+		// punto f
+		
+		vaciarCola(cola1);
+		llenarCola(cola1);
+		vaciarCola(cola2);
+		llenarCola(cola2);
+		
+		System.out.println("\nMostrando colas para inversa Negativo");
+		System.out.println("\nCola 1:\n");
+		mostrarCola(cola1);
+		System.out.println("\nCola 2:\n");
+		mostrarCola(cola2);
+		
+		if(inversa(cola1, cola2)){
+			System.out.println("\nLa colas son inversas");
+		}
+		else {
+			System.out.println("\nLa colas NO son inversas");
+		}
+		
+		cola2 = copiarCola(cola1);
+		invertirCola1(cola2);
+		
+		System.out.println("\nMostrando colas para inversa Positivo");
+		System.out.println("\nCola 1:\n");
+		mostrarCola(cola1);
+		System.out.println("\nCola 2:\n");
+		mostrarCola(cola2);
+		
+		if(inversa(cola1, cola2)) {
+			System.out.println("\nLa colas son inversas");
+		}
+		else {
+			System.out.println("\nLa colas NO son inversas");
+		}
 		
 	}
 }
--- tp1colaprio/src/uso/uso.java
+++ tp1colaprio/src/uso/uso.java
@@ -6,8 +6,8 @@ public class uso {
 	
 	public static void llenarCola(ColaPrioridad cola) {
 		
-		for(int i = 0; i <= 5; i++) {
-			cola.acolarPrioridad(i, (int)(Math.random()*11));	
+		for(int i = 0; i < 5; i++) {
+			cola.acolarPrioridad((int)(Math.random()*11), (int)(Math.random()*11));	
 		}
 	
 	}
@@ -32,14 +32,69 @@ public class uso {
 		while(!Colaaux.colaVacia());
 		
 	}
+	
+	public static void vaciarCola(ColaPrioridad cola) {	
+		while(!cola.colaVacia()) {cola.desacolar();}	
+	}
+	
+	public static ColaPrioridad copiarCola(ColaPrioridad cola) {
+		
+		ColaPrioridad Colaaux = new ColaPrioridad();
+		ColaPrioridad Colacopia = new ColaPrioridad();
+		Colaaux.inicializarCola();
+		Colacopia.inicializarCola();
+		int elemento = 0;
+		int prioridad = 0;
+
+		while(!cola.colaVacia()) {
+			elemento = cola.primero();
+			prioridad = cola.prioridad();
+			Colaaux.acolarPrioridad(elemento, prioridad);
+			Colacopia.acolarPrioridad(elemento, prioridad);
+			cola.desacolar();
+
+		};
+		
+		while(!Colaaux.colaVacia()) {
+			cola.acolarPrioridad(Colaaux.primero(), Colaaux.prioridad());
+			Colaaux.desacolar();
+		};
+		
+		return Colacopia;
+		
+	}
 
 	public static ColaPrioridad unirColas(ColaPrioridad cola1, ColaPrioridad cola2) {
 		
 		ColaPrioridad colaunida = new ColaPrioridad();
 		colaunida.inicializarCola();
 		
-
+		while(!cola1.colaVacia()) {
+			colaunida.acolarPrioridad(cola1.primero(),cola1.prioridad());
+			cola1.desacolar();
+		}
+		while(!cola2.colaVacia()) {
+			colaunida.acolarPrioridad(cola2.primero(),cola2.prioridad());
+			cola2.desacolar();
+		}
+		
 		return colaunida;
+	}
+	
+	public static boolean igualdad(ColaPrioridad cola1, ColaPrioridad cola2) {
+		
+		ColaPrioridad colaaux1 = copiarCola(cola1);
+		ColaPrioridad colaaux2 = copiarCola(cola2);
+		
+		while(!colaaux1.colaVacia()) {
+			if(!(colaaux1.primero() == colaaux2.primero())) {
+				return false;
+			}
+			colaaux1.desacolar();
+			colaaux2.desacolar();
+		}
+		
+		return true;
 	}
 	
 	public static void main(String[] args) {
@@ -51,9 +106,53 @@ public class uso {
 	Cola2.inicializarCola();
 	
 	llenarCola(Cola1);
-	System.out.print("primer vista\n");
+	System.out.print("\nMostrando Cola 1:\n\n");
 	mostrarCola(Cola1);
-	System.out.print("segunda vista\n");
+	
+	llenarCola(Cola2);
+	System.out.print("\nMostrando Cola 2:\n\n");
+	mostrarCola(Cola2);
+	
+	//Punto a
+	
+	ColaPrioridad Colaunida = unirColas(Cola1,Cola2);
+	System.out.print("\nMostrando Cola unida:\n\n");
+	mostrarCola(Colaunida);
+	
+	//Punto b
+	
+	vaciarCola(Cola1);
+	llenarCola(Cola1);
+	vaciarCola(Cola2);
+	llenarCola(Cola2);
+	
+	System.out.println("\nMostrando Colas para igualdad Negativa");
+	System.out.println("\nCola 1:\n");
 	mostrarCola(Cola1);
+	System.out.println("\nCola 2:\n");
+	mostrarCola(Cola2);
+	
+	if(igualdad(Cola1, Cola2)){
+		System.out.println("\nLa Colas son iguales");
+	}
+	else {
+		System.out.println("\nLa Colas NO son iguales");
+	}
+	
+	Cola2 = copiarCola(Cola1);
+	
+	System.out.println("\nMostrando Colas para igualdad Positiva");
+	System.out.println("\nCola 1:\n");
+	mostrarCola(Cola1);
+	System.out.println("\nCola 2:\n");
+	mostrarCola(Cola2);
+	
+	if(igualdad(Cola1, Cola2)) {
+		System.out.println("\nLa Colas son iguales");
+	}
+	else {
+		System.out.println("\nLa Colas NO son iguales");
+	}
+	
 	}
 }
